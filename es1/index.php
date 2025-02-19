c78a16798d6f6ee2b4c7d019d9ce01f89994f0cf
lai2-24 <lai2-24@itismeucci.com>
2025-02-19 09:53:51 +0100

    deseriealizzazione da un file

--- es1/index.php
+++ es1/index.php
@@ -1,28 +1,7 @@
 <?php
 header("Content-Type: application/json");
 require_once 'Alunno.php';
-
-$voti = [ 
-           new voti ("matematica",7,"bravo ma non si applica"),
-           new voti ("italiano",8,"bravo bravo bravo")
-        ];
-
-$indirizzo = [ 
-                new indirizzo("Via del fossetto","5/a","50145"),
-                new indirizzo("Via targioni tozzetti","18","50144")
-              ];
-
-  
-$Alunni = [
-                new Alunno("Stefano","Cherubini",18,$voti[0],$indirizzo[0], true),
-                new Alunno("Tommaso","Dell'osa",18,$voti[1],$indirizzo[1], true)
-          ];
-//serializzare
-$alunni_serializzati = json_encode($Alunni);
-//stampo a video
-// echo json_encode($Alunni);
-
-//deserializzare
+$alunni_serializzati = file_get_contents("Alunni.json");
 $alunni2 = json_decode($alunni_serializzati,true);
 $Alunni = [];
 foreach($alunni2 as $a)
