ada5d57ec48bf24d72973889ca5c1fd4ed0ff952
lai2-24 <lai2-24@itismeucci.com>
2025-02-19 09:51:35 +0100

    deseriealizzazione parziale

--- es1/index.php
+++ es1/index.php
@@ -17,6 +17,19 @@ $Alunni = [
                 new Alunno("Stefano","Cherubini",18,$voti[0],$indirizzo[0], true),
                 new Alunno("Tommaso","Dell'osa",18,$voti[1],$indirizzo[1], true)
           ];
+//serializzare
+$alunni_serializzati = json_encode($Alunni);
+//stampo a video
+// echo json_encode($Alunni);
+
+//deserializzare
+$alunni2 = json_decode($alunni_serializzati,true);
+$Alunni = [];
+foreach($alunni2 as $a)
+{
+    $var = new Alunno($a["nome"],$a["cognome"],$a["eta"],"" , "", true);
+    array_push($Alunni, $var);
+}
 
 echo json_encode($Alunni);
 ?>
